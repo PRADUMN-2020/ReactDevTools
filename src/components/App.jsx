e3d8b73da9e4300d6f9bcaf7c88523161b110016
Pradumn Prasad <PRADUMNPRASAD883@GMAIL.COM>
2023-03-24 01:57:51 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://media.licdn.com/dms/image/D4D35AQEkXeJ_9k88_A/profile-framedphoto-shrink_400_400/0/1676393553235?e=1680202800&v=beta&t=rBAROdb9FnnnoapYyiEuu_H3DoM8zxsgVzdB8inAm1Q" />
+
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
